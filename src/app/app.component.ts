ba0c2f33cc0ffb5f31017e4c3fe225e9b059c99b
MohitTheProgrammer <chmohit27@gmail.com>
2024-07-20 18:01:01 +0530

    about component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component"
 import { FaceComponent } from "./components/face/face.component"
-
+import { AboutComponent } from "./components/about/about.component"
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FaceComponent],
+  imports: [RouterOutlet, NavbarComponent, FaceComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
